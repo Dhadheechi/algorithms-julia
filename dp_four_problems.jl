603d90609cc516707ac74585a0d9391b70d75f1e
dhadheechi <rdhadheechi@gmail.com>
2025-03-22 10:40:46 +0530

    Added the correct file for dp_four_problems

--- dp_four_problems.jl
+++ dp_four_problems.jl
@@ -4,6 +4,16 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ a488a4be-cccf-462e-8ea2-6a6e46570626
+md"
+You plan to attend a college fest that is n days long and has ai number of activities on
+the ith day. Attending the fest on one day will leave you tired and unable to attend
+the next day. However you wish to take part in as many activities as possible. For
+example, if n = 5 and the numbers of activities are 8,3,1,7,4 respectively, you would
+attend day 1 and day 4 to maximize the number of activities that you can participate
+in (15 in this case). Suggest an efficient algorithm to solve this problem, i.e. to find
+the set of days that maximize the number of activities."
+
 # ╔═╡ ccdf0c36-01b7-11f0-2b44-a5e3243f21ce
 function choosefest(A, i) # choose the days starting from the ith day
 	days = []
@@ -253,7 +263,121 @@ end
 # ╔═╡ 07fce232-8fde-415a-b630-2b2adf579fa0
 @time cut_rod_dp(p, length(p))
 
+# ╔═╡ 0f5abeea-914c-4ecb-8472-c6584f966589
+begin
+	M = [0 7 0 0; 8 0 0 2; 0 0 1 0; 0 0 10 0]
+grid = [
+    0   2   4   0   3   5   1   0   7   2
+    1   0   5   6   0   2   8   3   0   4
+    0   3   0   2   7   0   1   5   6   0
+    8   0   1   4   0   3   0   2   7   9
+    10  1   0   2   9   0   8   4   0   3
+    0   7   3   5   0   2   6   0   9   1
+    4   0   8   2   1   0   5   7   0   6
+    3   6   0   9   4   2   0   1   8   5
+    0   5   2   0   7   1   6   4   0   3
+    9   0   4   1   0   8   2   5   7   0
+]
+end
+
+# ╔═╡ 8119b9a9-1efa-4e0f-b0c6-3d7ee0b572a9
+function grid_choose(M, i,j)
+	m = size(M)[1]
+	n = size(M)[2]
+	if i == 1 && j == n
+		return M[i,j] 
+	elseif i == 1 #&& j != 4
+		return M[i,j] + grid_choose(M, i, j+1)
+	elseif j == n#&& i
+		return M[i,j] + grid_choose(M, i-1, j)
+
+	else
+		return M[i, j] + max(grid_choose(M, i, j+1), grid_choose(M, i-1, j))
+	end
+end
+
+# ╔═╡ 9020c8fb-bc99-46cc-a906-ce23fe754b77
+@time grid_choose(grid, size(grid)[1], 1)
+
+# ╔═╡ 37c88779-1311-4924-b61f-1fc8d33b1c45
+function grid_choose_dp(M)
+	m = size(M)[1]
+	n = size(M)[2]
+	C = zeros(size(M))
+	# outer loop from right to left; inner loop from top to down
+	for i in 1:m
+		C[i, n] = sum(M[1:i, n])
+	end
+	for j in 1:n-1
+		C[1, j] = sum(M[1, j:end])
+	end
+	for j in n-1:-1:1
+		for i in 2:m
+			C[i,j] = max(C[i,j+1], C[i-1, j]) + M[i,j]
+		end
+	end
+	return C[m,1]
+end
+		
+			
+
+# ╔═╡ 07bf4b9a-8e3a-424a-9945-fb10fe45671f
+@time grid_choose_dp(grid)
+
+# ╔═╡ 8ddeff27-14e7-410e-a8c2-1460a26a3474
+function win_prob(A, i) # prob of winning at least i matches out of n
+	n = length(A)
+	if i == 0 # prob of winning at least 0 matches is one
+		return 1
+	end
+	if n < i
+		return 0
+	end
+	if n == 1
+		if i == 1
+			return A[1] # the only element
+		# else # i think i can go negative here, that returns one too
+		# 	return 1 # probability of winning at least 0 matches is one
+		end
+	end
+	
+	win = A[1]*win_prob(A[2:end], i-1)
+	lose = (1-A[1])*win_prob(A[2:end], i)
+	return win + lose
+end
+
+# ╔═╡ 5b604e6f-7832-44de-84b0-c44187529920
+@time win_prob([0.6, 0.1, 0.3, 0.7, 0.4, 0.2, 0.9, 0.7, 0.45], 5)
+
+# ╔═╡ 5d535ca5-b0c9-402c-a3fc-0b59d161cdaa
+length([0.6, 0.1, 0.3, 0.7, 0.4, 0.2, 0.9, 0,7, 0.45])
+
+# ╔═╡ d2d2a365-94ac-44a6-891f-ad34cdc7c3bb
+function win_prob_dp(A) # we never call the function inside itself; ratther, we construct the solution through memoization
+	n = length(A)
+	W = zeros(n+1, n) # the columns: no of matches remaining; columns: no of matches we "at least" need to win
+	W[1,n] = 1 # prob of winning 0 matches
+	W[2,n] = A[n]
+	for j in n-1:-1:1
+		W[1,j] = 1
+		for i in 2:(n-j+2)
+			W[i,j] = (1-A[j])*W[i, j+1] + A[j]*W[i-1, j+1] # prob of winning at least j matches (i matches remaining)
+		end
+	end
+	return W[div(n,2)+2, 1]
+	
+end	
+	
+	
+
+# ╔═╡ 8bf3f237-e34f-4441-a67c-1ad263147740
+@time win_prob_dp([0.6, 0.1, 0.3, 0.7, 0.4, 0.2, 0.9, 0.7, 0.45])
+
+# ╔═╡ 852fd79d-607d-48f6-9a2e-c68d251c0d79
+
+
 # ╔═╡ Cell order:
+# ╟─a488a4be-cccf-462e-8ea2-6a6e46570626
 # ╠═ccdf0c36-01b7-11f0-2b44-a5e3243f21ce
 # ╠═e6a804bc-9c2d-4d3a-9d59-1c66d8413f9c
 # ╠═6f6285ae-ef2e-4a26-ae17-4f1525254f7d
@@ -278,3 +402,14 @@ end
 # ╠═6e4bbc22-fddd-4620-a708-c7399ccfd47f
 # ╠═d154b83d-0312-4351-b9d4-75e921a79150
 # ╠═07fce232-8fde-415a-b630-2b2adf579fa0
+# ╠═0f5abeea-914c-4ecb-8472-c6584f966589
+# ╠═8119b9a9-1efa-4e0f-b0c6-3d7ee0b572a9
+# ╠═9020c8fb-bc99-46cc-a906-ce23fe754b77
+# ╠═37c88779-1311-4924-b61f-1fc8d33b1c45
+# ╠═07bf4b9a-8e3a-424a-9945-fb10fe45671f
+# ╠═8ddeff27-14e7-410e-a8c2-1460a26a3474
+# ╠═5b604e6f-7832-44de-84b0-c44187529920
+# ╠═5d535ca5-b0c9-402c-a3fc-0b59d161cdaa
+# ╠═d2d2a365-94ac-44a6-891f-ad34cdc7c3bb
+# ╠═8bf3f237-e34f-4441-a67c-1ad263147740
+# ╠═852fd79d-607d-48f6-9a2e-c68d251c0d79
